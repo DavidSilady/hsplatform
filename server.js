4c984248da6825b8d165917ac631414da354c6fa
davidsilady <david.silady@gmail.com>
2020-11-04 13:45:13 +0100

    working session

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-const http = require('http')
+const express = require('express')
+const session = require('express-session')
 const fs = require('fs')
 const port = 3000
 
-const server = http.createServer(function (req, res) {
+const app = express()
 
-    fs.readFile('index.html', function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-            res.end();
-            console.log(error)
-        }
-        else if (req.url === '/index.html'){
-            res.writeHead(
-                200,
-                {'Content-Type': 'text/html' }
-            );
-            res.write(data);
-            res.end();
-        } else {
-            res.write('No good');
-            res.end();
-        }
-    })
-})
-
-server.listen(port, function (error) {
-    if (error) {
-        console.log('Something went wrong', error);
-    } else {
-        console.log('Server is listening on port ' + port);
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: 'key',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
     }
-})
+}));
+
+app.get('/', standardExpressCallback)
+app.use(errorHandler)
+
+function standardExpressCallback (req, res, next) {
+    if (req.session.viewCount) {
+        console.log('View Count Up')
+        req.session.viewCount += 1;
+    } else {
+        req.session.viewCount = 1;
+    }
+    res.status(200).send( `<h1>Hello There ${req.session.viewCount}</h1>`)
+}
+
+function errorHandler(err, req, res, next) {
+    if (err) {
+        console.log(err)
+        res.status(400).send('<h2>There has been an error.</h2>')
+    }
+}
+
+app.listen(port)
